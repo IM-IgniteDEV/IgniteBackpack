c96a86a02e183e43ff66b5d0c29681f2a2cf99d8
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-12 16:37:46 +0200

    BackPack data

---
+++ src/main/java/com/ignitedev/igniteBackpacks/base/BackpackData.java
@@ -0,0 +1,59 @@
+package com.ignitedev.igniteBackpacks.base;
+
+import com.comphenix.protocol.events.PacketContainer;
+import lombok.Data;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Data
+public class BackpackData {
+  private final UUID owner;
+  private int armorStandId;
+  private BukkitTask updateTask;
+  private List<PacketContainer> spawnPackets = new ArrayList<>();
+  private PacketContainer passengerPacket;
+  private List<UUID> visibleTo = new ArrayList<>();
+
+  public BackpackData(Player player) {
+    this.owner = player.getUniqueId();
+    visibleTo.add(owner);
+  }
+
+  public Player getPlayer() {
+    return Bukkit.getPlayer(owner);
+  }
+
+  public List<Player> getVisibleToPlayers() {
+    return visibleTo.stream()
+        .map(Bukkit::getPlayer)
+        .filter(Objects::nonNull)
+        .toList();
+  }
+
+  public void addSpawnPacket(PacketContainer packet) {
+    this.spawnPackets.add(packet);
+  }
+
+  public void addVisibleTo(UUID uuid) {
+    if (!visibleTo.contains(uuid)) {
+      visibleTo.add(uuid);
+    }
+  }
+
+  public void removeVisibleTo(UUID uuid) {
+    if (!owner.equals(uuid)) {
+      visibleTo.remove(uuid);
+    }
+  }
+
+  public List<PacketContainer> getSpawnPackets() {
+    return new ArrayList<>(spawnPackets);
+  }
+
+}
