aa7427e3b6b424ccf78608ee0837f9d006cd1d53
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-14 16:55:47 +0200

    Created example backpack itemstack models in configuration

--- src/main/java/com/ignitedev/igniteBackpacks/config/BackpackConfig.java
+++ src/main/java/com/ignitedev/igniteBackpacks/config/BackpackConfig.java
@@ -4,15 +4,51 @@ import com.ignitedev.igniteBackpacks.base.ModelData;
 import com.twodevsstudio.simplejsonconfig.api.Config;
 import com.twodevsstudio.simplejsonconfig.interfaces.Comment;
 import com.twodevsstudio.simplejsonconfig.interfaces.Configuration;
+import de.tr7zw.nbtapi.NBT;
 import java.util.List;
 import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 @SuppressWarnings("FieldMayBeFinal")
 @Getter
-@Configuration("config")
+@Configuration("backpacksConfiguration.json")
 public class BackpackConfig extends Config {
 
-  private List<ModelData> supportedModels = example();
+  private List<ModelData> supportedModels =
+      List.of(
+          new ModelData("Test", 1111111, 1111112, 1111113),
+          new ModelData("Test2", 2222222, 2222223, 2222224));
+
+  private List<ItemStack> supportedBackpacksItem = exampleBackpacks();
+
+  public List<ItemStack> exampleBackpacks() {
+    ItemStack itemStackOne = new ItemStack(Material.LEATHER);
+    ItemStack itemStackTwo = new ItemStack(Material.DISC_FRAGMENT_5);
+
+    NBT.modify(
+        itemStackOne,
+        nbt -> {
+          nbt.setInteger("level", 1);
+          nbt.modifyMeta(
+              (readableNBT, itemMeta) -> {
+                itemMeta.setCustomModelData(1111111);
+                itemMeta.setDisplayName("Backpack Level 1");
+              });
+        });
+    NBT.modify(
+        itemStackTwo,
+        nbt -> {
+          nbt.setInteger("level", 2);
+          nbt.modifyMeta(
+              (readableNBT, itemMeta) -> {
+                itemMeta.setCustomModelData(2222222);
+                itemMeta.setDisplayName("Backpack Level 2");
+                itemMeta.setLore(List.of("&7This backpack is bigger than level one, each level gives you 9 slots more!"));
+              });
+        });
+    return List.of(itemStackOne, itemStackTwo);
+  }
 
   @Comment("Render distance in blocks for armor stand packets")
   private int backpackRenderDistanceBlocks = 30;
@@ -47,9 +83,5 @@ public class BackpackConfig extends Config {
 
   public boolean isSupported(int modelId) {
     return getByModelId(modelId) != null;
-  }
-
-  private List<ModelData> example() {
-    return List.of(new ModelData("Test", 1111111, 1111112, 1111113));
   }
 }
