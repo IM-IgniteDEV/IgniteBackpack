6954369354f392323958059f30826f79eaaa7e1f
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-14 20:20:55 +0200

    Simplified backpack equip listener

--- src/main/java/com/ignitedev/igniteBackpacks/listener/EquipBackpackListener.java
+++ src/main/java/com/ignitedev/igniteBackpacks/listener/EquipBackpackListener.java
@@ -1,18 +1,16 @@
 package com.ignitedev.igniteBackpacks.listener;
 
-import com.ignitedev.aparecium.util.MessageUtility;
+import com.ignitedev.igniteBackpacks.IgniteBackpacks;
 import com.ignitedev.igniteBackpacks.config.BackpackConfig;
 import com.ignitedev.igniteBackpacks.event.PlayerArmorChangeEvent;
 import com.ignitedev.igniteBackpacks.packet.ArmorStandPacketManager;
 import com.ignitedev.igniteBackpacks.util.BackpackUtility;
 import com.twodevsstudio.simplejsonconfig.interfaces.Autowired;
 import lombok.RequiredArgsConstructor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
 @RequiredArgsConstructor
 public class EquipBackpackListener implements Listener {
@@ -21,8 +19,10 @@ public class EquipBackpackListener implements Listener {
 
   private final ArmorStandPacketManager packetManager;
 
+  private final IgniteBackpacks plugin;
+
   @EventHandler
-  public void onRightClick(PlayerArmorChangeEvent event) {
+  public void onEquip(PlayerArmorChangeEvent event) {
     if (event.getSlot() != EquipmentSlot.CHEST) {
       return;
     }
@@ -32,18 +32,6 @@ public class EquipBackpackListener implements Listener {
       return;
     }
     event.setCancelled(true);
-
-    Player player = event.getPlayer();
-    PlayerInventory inventory = player.getInventory();
-
-    if (inventory.getChestplate() != null) {
-      MessageUtility.send(player, config.getPrefix() + config.getCannotEquipBackpackMessage());
-      return;
-    }
-    packetManager.attachBackpackToPlayer(player);
-    packetManager.updateBackpackAppearance(player, item.clone());
-
-    inventory.setChestplate(item.clone());
-    inventory.setItemInMainHand(null);
+    BackpackUtility.updateBackpack(event.getPlayer(), plugin, packetManager);
   }
 }
